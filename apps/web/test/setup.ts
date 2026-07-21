9f4fa9202f3b92ee25216aee20c749443b605535
Meet Solanki <solankimeet518@gmail.com>
2026-07-21 16:45:35 +0530

    Add: add setViewportWidth helper for matchMedia tests

--- apps/web/test/setup.ts
+++ apps/web/test/setup.ts
@@ -1,1 +1,21 @@
 import '@testing-library/jest-dom/vitest'
+
+export function setViewportWidth(width: number) {
+  Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    configurable: true,
+    value: (query: string) => ({
+      matches: width >= 768,
+      media: query,
+      onchange: null,
+      addListener: () => { },
+      removeListener: () => { },
+      addEventListener: () => { },
+      removeEventListener: () => { },
+      dispatchEvent: () => false,
+    }),
+  });
+}
+
+setViewportWidth(1024);
+
